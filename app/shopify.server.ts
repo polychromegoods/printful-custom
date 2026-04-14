f1186dc54bc9da85b1eba278fcb7026e53957113
polychromegoods <220055671+polychromegoods@users.noreply.github.com>
2026-04-14 12:58:37 -0400

    fix: use AppDistribution.Custom for Partners custom app

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -15,7 +15,7 @@ const shopify = shopifyApp({
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
-  distribution: AppDistribution.ShopifyAdmin,
+  distribution: AppDistribution.Custom,
   future: {
     unstable_newEmbeddedAuthStrategy: true,
     expiringOfflineAccessTokens: true,
